ca209a10a27efc5b557a18756936170c9b0d9e6b
richardhansel <94770096+richardhansel@users.noreply.github.com>
2021-12-27 17:01:20 -0500

    Add files via upload

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name:  'Macbook',
+            price: 1000,
+            image: 'https://picsum.photos/id/180/240/160',
+        },
+        {
+            id: '2',
+            name:  'Old Car',
+            price: 2500,
+            image: 'https://picsum.photos/id/111/240/160',
+        },
+        {
+            id: '3',
+            name: 'Women Shoes',
+            price: 500,
+            image: 'https://picsum.photos/id/21/240/160',
+        },
+    ],
+};
+export default data;
